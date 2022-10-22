7e92b9bc467d173bfe714906db4fc769772b085d
TheKiesling <kie21581@uvg.edu.gt>
2022-10-21 23:54:59 -0600

    login

---
+++ app/src/main/java/com/example/monni/LoginFragment.kt
@@ -0,0 +1,13 @@
+package com.example.monni
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+
+}
---
+++ app/src/main/java/com/example/monni/SavingsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.monni
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SavingsFragment : Fragment(R.layout.fragment_savings) {
+
+}
