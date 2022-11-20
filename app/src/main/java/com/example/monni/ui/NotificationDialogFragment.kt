6a8bd8a7343f2bb21e52ca167451a7cff9712913
TheKiesling <kie21581@uvg.edu.gt>
2022-11-19 21:40:32 -0600

    new notification

--- app/src/main/java/com/example/monni/ui/NotificationDialogFragment.kt
+++ app/src/main/java/com/example/monni/ui/NotificationDialogFragment.kt
@@ -5,13 +5,24 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import androidx.room.Room
+import com.example.monni.data.local.entity.Notification
+import com.example.monni.data.local.source.CategoryDatabase
+import com.example.monni.data.local.storage.DataStorage
 import com.example.monni.databinding.FragmentGoalDialogBinding
 import com.example.monni.databinding.FragmentNotificationDialogBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NotificationDialogFragment(): DialogFragment() {
     private lateinit var binding :FragmentNotificationDialogBinding
+    private lateinit var dataStore: DataStorage
+    private lateinit var id: String
+    private lateinit var categoryDatabase: CategoryDatabase
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = FragmentNotificationDialogBinding.inflate(LayoutInflater.from(context))
@@ -25,9 +36,35 @@ class NotificationDialogFragment(): DialogFragment() {
         return dialog
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        dataStore = DataStorage(requireContext())
+        categoryDatabase = Room.databaseBuilder(
+            requireContext(),
+            CategoryDatabase::class.java,
+            "dbname"
+        ).build()
+    }
+
     private fun setListeners() {
         binding.apply {
             saveButtonNotificationDialog.setOnClickListener{
+                val dateLimit = binding.textInputDateNotificationDialog.editText!!.text.toString()
+                val title = binding.textInputTitleNotificationDialog.editText!!.text.toString()
+                val desc = binding.textInputDescNotificationDialog.editText!!.text.toString()
+                CoroutineScope(Dispatchers.IO).launch {
+                    id = dataStore.getValueFromKey("email")!!
+                }
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    categoryDatabase.notificationDao().insert(
+                        Notification(
+                            id = id,
+                            dateLimit = dateLimit,
+                            title = title,
+                            desc = desc
+                        )
+                    )
+                }
                 dismiss()
             }
             cancelButtonNotificationDialog.setOnClickListener{
