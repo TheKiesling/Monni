91210b558fd1ba92fbef5c4d2d0a6b66afea292a
Andrea-gt <a.ximeram@gmail.com>
2022-11-22 04:39:18 -0600

    Pie Done

--- app/src/main/java/com/example/monni/ui/PieChartFragment.kt
+++ app/src/main/java/com/example/monni/ui/PieChartFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.example.monni.R
 import com.example.monni.data.local.entity.Category
+import com.example.monni.data.local.source.CategoryDatabase
+import com.example.monni.data.local.source.InitializerSavingTips
 import com.example.monni.database.Database
 import com.example.monni.databinding.FragmentPieChartBinding
 import com.github.mikephil.charting.animation.Easing
@@ -19,16 +24,20 @@ import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class PieChartFragment : Fragment(R.layout.fragment_pie_chart) {
+    private val args: PieChartFragmentArgs by navArgs()
     private lateinit var recyclerView: RecyclerView
     private val listOfEntries: MutableList<PieEntry> = arrayListOf()
     private val listOfColors: MutableList<Int> = arrayListOf()
     private lateinit var pieChart: PieChart
     private lateinit var binding: FragmentPieChartBinding
-    private var database: Database = Database
-    private lateinit var categoriesList: List<Category>
+    private lateinit var database: CategoryDatabase
+    private var categoriesList: List<Category> = listOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,27 +50,51 @@ class PieChartFragment : Fragment(R.layout.fragment_pie_chart) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        database = Room.databaseBuilder(
+            requireContext(),
+            CategoryDatabase::class.java,
+            "dbname"
+        ).build()
+
         pieChart = binding.expensesPieChart
         recyclerView = binding.recyclerViewExpensesPieFragment
-        setupRecyclers()
-        setUpPieChart("the_kiesling")
+        setInfo()
+        setUpPieChart(args.email)
     }
 
-    private fun setUpPieChart(username: String){
-        for(category in database.getCategories(username)) {
-            listOfColors.add(ColorTemplate.rgb(category.color))
-            val totalOfCategories: Double = database.getTotalCategories(username)
-            listOfEntries.add(
-                PieEntry(
-                    (category.amount / totalOfCategories).toFloat(),
-                    category.name
-                )
-            )
+    private fun setUpPieChart(username: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            for (category in database.categoryDao().getCategories(args.email)) {
+                listOfColors.add(ColorTemplate.rgb(category.color))
+            }
+            var totalOfCategories = 0.00
+            for (category in database.categoryDao().getCategories(args.email)) {
+                totalOfCategories += category.amount
+                if (totalOfCategories.toInt() != 0) {
+                    listOfEntries.add(
+                        PieEntry(
+                            (category.amount / totalOfCategories).toFloat(),
+                            category.name
+                        )
+                    )
+                    
+                    CoroutineScope(Dispatchers.Main).launch {
+                        declarePieChartData(listOfEntries, listOfColors, pieChart)
+                    }
+                } else {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        Toast.makeText(requireContext(), "Sin registros", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                    break
+                }
+            }
         }
-        declarePieChartData(listOfEntries,listOfColors,pieChart)
     }
+
+
     private fun setupRecyclers(){
-            categoriesList = database.getCategories("the_kiesling")
             recyclerView.layoutManager = LinearLayoutManager(requireContext())
             recyclerView.setHasFixedSize(true)
             recyclerView.adapter = PieChartAdapter(categoriesList)
@@ -82,6 +115,16 @@ class PieChartFragment : Fragment(R.layout.fragment_pie_chart) {
         pieChart.invalidate()
         pieChart.animateY(1400, Easing.EaseInOutQuad)
 
+    }
+
+    private fun setInfo(){
+        CoroutineScope(Dispatchers.IO).launch {
+            var categories = database.categoryDao().getCategories(args.email)
+            categoriesList = categories
+            CoroutineScope(Dispatchers.Main).launch {
+                setupRecyclers()
+            }
+        }
     }
 }
 
